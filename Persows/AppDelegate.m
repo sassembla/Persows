df05250dc6dd85570c50ced4ab60aa77282d1987
Toru <toru.inoue@kissaki.tv>
2013-11-22 15:18:24 +0900

    latestを引っ張るのまで把握

--- Persows/AppDelegate.m
+++ Persows/AppDelegate.m
@@ -41,28 +41,43 @@ enum PE_MASTER_EXEC {
 - (void) receiver:(NSNotification * )notif {
     switch ([messenger execFrom:[messenger myName] viaNotification:notif]) {
         case PE_MASTER_EXEC_INIT:{
-            [self save];
+            [self loadData];
             
             break;
         }
             
         default:
+            NSAssert(false, @"should not reach here");
             break;
     }
 }
 
+- (void) loadData {
+    PFQuery * query = [PFQuery queryWithClassName:@"TestObject"];
 
+    
+    // これはlatestを引っ張る
+    [query getFirstObjectInBackgroundWithBlock:^(PFObject *gameScore, NSError *error) {
+        // Do something with the returned PFObject in the gameScore variable.
+        NSLog(@"%@", gameScore);
+    }];
+    
+    
+//    [query getObjectInBackgroundWithId:@"xVMGPMJnOX" block:^(PFObject *gameScore, NSError *error) {
+//        // Do something with the returned PFObject in the gameScore variable.
+//        NSLog(@"%@", gameScore);
+//    }];
+}
 
 /**
  保存を行う
  */
 - (void) save {
-    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    PFObject * testObject = [PFObject objectWithClassName:@"TestObject"];
     [testObject setObject:@"bar" forKey:@"foo"];
     
     NSError * error;
     bool result = [testObject save:&error];
-    
 }
 
 @end
